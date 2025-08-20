419bd99ceebf1c9ed15df16882479b908c5c3094
ryujikang <dbwlrkd2000@naver.com>
2025-08-20 11:01:36 +0900

    실습완료

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -14,35 +14,41 @@ package com.nhnacademy;
 
 import com.nhnacademy.thread.CounterHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.management.monitor.CounterMonitor;
 import javax.management.monitor.Monitor;
 
 @Slf4j
-public class App
-{
+public class App {
 
+    private static final Logger log = LoggerFactory.getLogger(App.class);
     //TODO#1 monitor로 사용한 객체를 생성 합니다.
-    public static Object monitor;
+    public static Object monitor = new Object(); // 단순 Object 객체가 모니터 역할을 함
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws InterruptedException {
 
         //TODO#2 counterHandlerA 객체를 생성 합니다. countMaxSize : 10, monitor
-        CounterHandler counterHandlerA = null;
+        CounterHandler counterHandlerA = new CounterHandler(10, monitor); // 모니터 객체를 넘김
 
         //threadA 생성시 counterHandlerA 객체를 paramter로 전달 합니다.
         Thread threadA = new Thread(counterHandlerA);
 
         //threadA의 name을 'my-counter-A' 로 설정 합니다.
         threadA.setName("my-counter-A");
-        log.debug("threadA-state:{}",threadA.getState());
+        log.debug("threadA-state:{}",threadA.getState()); // 상태 : New
 
         //threadA를 시작 합니다.
         threadA.start();
-        log.debug("threadA-state:{}",threadA.getState());
+        log.debug("threadA-state:{}",threadA.getState()); // 상태 : RUNNABLE
+
 
         //TODO#3 - Main Thread에서 2초 후 monitor를 이용하여 대기하고 있는 threadA를 깨움 니다.
+        Thread.sleep(2000); // 2초 후
+        synchronized (monitor) { // 동기화 블럭 (monitor는 synchronized 블럭안에서 사용해야함)
+            monitor.notify(); // monitor로 treadA를 깨움(notify) : WAIT -> RUNNABLE
+        }
 
 
         //Main Thread가 threadA  종료될 때 까지 대기 합니다. Thread.yield를 사용 합니다.
--- src/main/java/com/nhnacademy/thread/CounterHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterHandler.java
@@ -13,9 +13,12 @@
 package com.nhnacademy.thread;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Slf4j
 public class CounterHandler implements Runnable  {
+    private static final Logger log = LoggerFactory.getLogger(CounterHandler.class);
     private final Object monitor;
     private final long countMaxSize;
 
@@ -23,17 +26,27 @@ public class CounterHandler implements Runnable  {
 
     public CounterHandler(long countMaxSize, Object monitor) {
         //TODO#4 countMaxSize<=0 or monitor 객체가 null 이면 IllegalArgumentException이 발생 합니다.
-
+        if (countMaxSize <= 0 || monitor == null) {
+            throw new IllegalArgumentException("객체 생성 불가");
+        }
 
         //TODO#5  countMaxSize, count, monitor 변수를 초기화 합니다.
-
+        this.countMaxSize = countMaxSize;
+        this.monitor = monitor; // 모니터 초기화
+        count = 0;
     }
 
     @Override
     public void run() {
         //TODO#6 Thread에 의해서 run() method가 호출되면 무한 대기 합니다. monitor객체를 이용해서 구현하세요
         //monitor는 여러 Thread가 동시에 접근할 수 없도록  접근을 제어해야 합니다.
-
+        synchronized (monitor) {
+            try {
+                monitor.wait(); // monitor가 깨울때까지 대기 -> WAIT 상태
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } // notify로 깨우면 -> RUNNABLE 상태로 바뀌고 반복문 실행
 
         do {
             try {
@@ -42,7 +55,7 @@ public class CounterHandler implements Runnable  {
                 throw new RuntimeException(e);
             }
             count++;
-            log.debug("thread:{},state:{},count:{}",Thread.currentThread().getName(),Thread.currentThread().getState(),count);
+            log.debug("thread:{}, state:{}, count:{}", Thread.currentThread().getName(), Thread.currentThread().getState(), count); // RUNNABLE 상태 (Thread에 정의(enum)되있는 상태 = NEW, RUNNABLE, BLOCKED, WAITING, TIMED_WAITING, TERMINATED)
 
         }while (count<countMaxSize);
     }
