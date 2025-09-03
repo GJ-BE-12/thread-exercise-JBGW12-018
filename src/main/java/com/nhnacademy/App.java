314ddac6e5497e281f7927e5563f59e9560397bb
ryujikang <dbwlrkd2000@naver.com>
2025-09-03 09:52:21 +0900

    수정

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -15,21 +15,18 @@ package com.nhnacademy;
 import com.nhnacademy.count.SharedCounter;
 import com.nhnacademy.thread.CounterIncreaseHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Slf4j
 public class App {
-    private static final Logger log = LoggerFactory.getLogger(App.class);
 
     public static void main( String[] args ) {
         Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
 
         //shardCounter 객체를 0으로 초기화 합니다.
         SharedCounter sharedCounter = new SharedCounter(0l);
-
         //counterIncreaseHandler 객체를 생성 합니다.
         CounterIncreaseHandler counterIncreaseHandler = new CounterIncreaseHandler(sharedCounter);
+
         //counterIncreaseHandler를 이용해서 threadA를 생성 합니다.
         Thread threadA = new Thread(counterIncreaseHandler);
         //threadA의 thread name을 "thread-A"로 설정 합니다.
@@ -41,7 +38,6 @@ public class App {
         Thread threadB = new Thread(counterIncreaseHandler);
         //threadB의 name을 'thread-B' 로 설정 합니다.
         threadB.setName("thread-B");
-
         //threadB를 시작 합니다.
         threadB.start();
 
--- src/main/java/com/nhnacademy/count/SharedCounter.java
+++ src/main/java/com/nhnacademy/count/SharedCounter.java
@@ -31,12 +31,16 @@ public class SharedCounter {
            ReentrantLock은 기본적으로 비공정한 락 입니다. 공정성을 보장 하도록 초기화 합니다.
          */
         mutex = new ReentrantLock(true); // Reentrant: '안으로 들어가기'
-                                              //    true = fair 모드
-                                              //    false(or 인자없이) = unfair 모드
+        // fair 모드 : true >> "FIFO (First-In-First-Out) 순서로 락을 대기"하는 스레드에 락을 부여
+        //      - 기아 현상(starvation)을 방지 가능
+        //      - 성능이 약간 저하 (공정한 락은 스레드 간의 컨텍스트 스위칭이 더 자주 발생할 수 있음)
+        // unfair 모드 : false(default) >> 락을 요청하는 순서와 상관이 없고 "현재 락을 해제한 스레드가 다시 락을 획득할 기회가 높다"고 알려져 있음 (스레드 스케줄링 구현 방식에 따라 달라질 수 있음)
+        //      - 특정 스레드가 락을 오랫동안 획득하지 못하는 기아 현상(Starvation)이 발생 가능
+
         // mutex = semaphore(1) ..?
 
-        // mutex : 한번에 하나만 (소유권 O, lock설정한 스레드만 unlock 가능)
         // semaphore : 허용된 개수만큼 (소유권 X, 다른 스레드도 release 가능)
+        // mutex : 한번에 하나만 (소유권 O, lock을 건 스레드만 unlock 가능)
     }
 
     public long getCount(){
@@ -47,12 +51,12 @@ public class SharedCounter {
             잠금을 해제 합니다. 뮤텍스는 lock을 건 쓰레드만 lock을 해제할 수 있습니다.
          */
         try {
-            mutex.lock(); // lock 설정
+            mutex.lock(); // lock 설정 (만약 다른 스레드가 이미 뮤텍스를 잠근 상태라면, 현재 스레드는 뮤텍스가 해제될 때까지 대기)
             return count;
         } catch (RuntimeException e) {
             throw new RuntimeException();
         } finally {
-            mutex.unlock(); // lock 해제
+            mutex.unlock(); // lock 해제 - lock을 건 스레드만 해제 가능 (여러 스레드가 동시에 임계 구역에 접근하는 것을 방지, 공유 자원의 일관성과 무결성을 유지 가능)
         }
     }
 
